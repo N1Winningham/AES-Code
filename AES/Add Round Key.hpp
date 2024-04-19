7ff41579ed7c7e2791ff2a7a40158e498c1652fc
Nathan Winningham <130709664+N1Winningham@users.noreply.github.com>
2024-04-19 11:14:46 -0400

    Add files via upload

--- AES/Add Round Key.hpp
+++ AES/Add Round Key.hpp
@@ -2,31 +2,121 @@
 // Created by Nathan Winningham on 3/10/24.
 //
 
-//WIP, includes pseudocode.
+//Includes pseudocode for future implementation.
 
 
-#include <string>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include "Substitute Bytes.hpp"
 using namespace std;
 
 #ifndef AES_KEY_SCHEDULE_HPP
 #define AES_KEY_SCHEDULE_HPP
 
-void addRoundKey() {
-    if (currentRound == 1) {
-        XOR(subkey, state);
-    } else {
-        XOR(mainKey, state);
+class addRoundKey{
+public:
+
+    SubBytes subB;
+
+    //Rotate for the key expansion.
+    void rotateKey(unsigned char *column) {
+        int cTemp = column[0];
+        column[0] = column[1];
+        column[1] = column[2];
+        column[2] = column[3];
+        column[3] = cTemp;
     }
-    nextRound;
-}
+
+    //Generates the fully expanded key.
+    unsigned char generateFullKey(unsigned char* originalKey, unsigned char fullKey[4][60]) {
+        //Fills the first four-by-four of the full key with the original key.
+        for (int i=0; i<16; i++) {
+            fullKey[i/4][i%4] = originalKey[i]; //Fills top to bottom, then left to right.
+        }
+
+        //Creates a temporary one-by-four colum used to fill the next column in the full key.
+        unsigned char temp[4];
+
+        //Fills in the rest of the full key.
+        for (int i=4; i<60; i++) {
+            //Copies the full key column into the temp.
+            for (int j=0; j<4; j++) {
+                temp[j] = fullKey[j][i-1];
+            }
+
+            //Applies the rotate, subBytes, and rcon steps if the round is creating a new round key matrix.
+            if (i%4 == 0) {
+                rotateKey(temp);
+                subB.SBoxKey(temp);
+                temp[0] = temp[0] ^ rcon[i/4-1][0];
+            }
+
+            //XORs the temp column with the full key value with the column that is four to the left of the newly inserted
+            // column.
+            for (int j=0; j<4; j++) {
+                fullKey[j][i] = fullKey[j][i-4] ^ temp[j];
+            }
+        }
+        return fullKey[0][0];
+    }
+
+    //Declaring all the individual round key matrices.
+    unsigned char InitialRoundKey[16];
+    unsigned char RoundKey1[16];
+    unsigned char RoundKey2[16];
+    unsigned char RoundKey3[16];
+    unsigned char RoundKey4[16];
+    unsigned char RoundKey5[16];
+    unsigned char RoundKey6[16];
+    unsigned char RoundKey7[16];
+    unsigned char RoundKey8[16];
+    unsigned char RoundKey9[16];
+    unsigned char RoundKey10[16];
+
+    //For AES-192.
+//    unsigned char InitialRoundKey[24];
+//    unsigned char RoundKey1[24];
+//    unsigned char RoundKey2[24];
+//    unsigned char RoundKey3[24];
+//    unsigned char RoundKey4[24];
+//    unsigned char RoundKey5[24];
+//    unsigned char RoundKey6[24];
+//    unsigned char RoundKey7[24];
+//    unsigned char RoundKey8[24];
+//    unsigned char RoundKey9[24];
+//    unsigned char RoundKey10[24];
+//    unsigned char RoundKey11[24];
+//    unsigned char RoundKey12[24];
+
+    //For AES-256
+//    unsigned char InitialRoundKey[32];
+//    unsigned char RoundKey1[32];
+//    unsigned char RoundKey2[32];
+//    unsigned char RoundKey3[32];
+//    unsigned char RoundKey4[32];
+//    unsigned char RoundKey5[32];
+//    unsigned char RoundKey6[32];
+//    unsigned char RoundKey7[32];
+//    unsigned char RoundKey8[32];
+//    unsigned char RoundKey9[32];
+//    unsigned char RoundKey10[32];
+//    unsigned char RoundKey11[32];
+//    unsigned char RoundKey12[32];
+//    unsigned char RoundKey13[32];
+//    unsigned char RoundKey14[32];
+
+    //Function used to break up the full key.
+    void roundKeyBreak(unsigned char fullKey[4][60],unsigned char * roundKey, int startColumn) {
+        int index = 0;
+        for (int col = 0; col < 4; col++) {
+            for (int row = 0; row < 4; row++) {
+                roundKey[col + row * 4] = fullKey[row][startColumn + col];
+            }
+        }
+    }
 
 
-void rcon(currentRound, rconTemp){
-    vector<vector<int>> rcon = {
+    //Array of arrays which stand for the rows of the rcon table.
+    const unsigned char rcon[14][4] = {
             {0x01, 0x00, 0x00, 0x00},
             {0x02, 0x00, 0x00, 0x00},
             {0x04, 0x00, 0x00, 0x00},
@@ -37,52 +127,13 @@ void rcon(currentRound, rconTemp){
             {0x80, 0x00, 0x00, 0x00},
             {0x1b, 0x00, 0x00, 0x00},
             {0x36, 0x00, 0x00, 0x00},
-            {0x6c, 0x00, 0x00, 0x00},
-            {0xd8, 0x00, 0x00, 0x00},
-            {0xab, 0x00, 0x00, 0x00},
-            {0x4d, 0x00, 0x00, 0x00}
+            //For AES-192 and AES-256.
+//            {0x6c, 0x00, 0x00, 0x00},
+//            {0xd8, 0x00, 0x00, 0x00},
+//            {0xab, 0x00, 0x00, 0x00},
+//            {0x4d, 0x00, 0x00, 0x00}
     };
-    if(currentRound == 1){
-        rconTemp
-    }
-}
+};
 
-void rotate(vector<int> state,int colNum){
-    vector<int> vTemp;
-//    for(int i=4;i<8;i++){
-    vTemp.push_back(state[colNum]);
-    swap(state[colNum], state[colNum+4]);
-    swap(state[colNum+4], state[colNum+8]);
-    swap(state[colNum+8], state[colNum+12]);
-    state[colNum+12] = vTemp[colNum];
-    vTemp.clear();
-//    }
-//    return state;
-}
-
-void innerLoop(vector<int> state){
-    vector<vector<int>> rcon = {
-            {0x01, 0x00, 0x00, 0x00},
-            {0x02, 0x00, 0x00, 0x00},
-            {0x04, 0x00, 0x00, 0x00},
-            {0x08, 0x00, 0x00, 0x00},
-            {0x10, 0x00, 0x00, 0x00},
-            {0x20, 0x00, 0x00, 0x00},
-            {0x40, 0x00, 0x00, 0x00},
-            {0x80, 0x00, 0x00, 0x00},
-            {0x1b, 0x00, 0x00, 0x00},
-            {0x36, 0x00, 0x00, 0x00},
-            {0x6c, 0x00, 0x00, 0x00},
-            {0xd8, 0x00, 0x00, 0x00},
-            {0xab, 0x00, 0x00, 0x00},
-            {0x4d, 0x00, 0x00, 0x00}
-    };
-
-    rotate(state, 4);
-    unsigned char currentByte = hex(rcon[1]);
-    SBox();
-
-
-}
 
 #endif //AES_ADD_ROUND_KEY_HPP
--- AES/Shift Rows.hpp
+++ AES/Shift Rows.hpp
@@ -2,45 +2,46 @@
 // Created by Nathan Winningham on 3/10/24.
 //
 
-//WIP, includes pseudocode.
-
 #include <iostream>
 
 #ifndef AES_SHIFT_ROWS_HPP
 #define AES_SHIFT_ROWS_HPP
+class ShiftRows {
+public:
+    unsigned char shiftingTemp = 0x00;
+    unsigned char shiftRow(unsigned char state[16], int row_num){
+        //If it is the second row it shifts all the values so that the first value of the row is moved to the end and the
+        //rest are shifted to the left one.
+        if (row_num == 2){
+            shiftingTemp = state[4];
+            state[4] = state[5];
+            state[5] = state[6];
+            state[6] = state[7];
+            state[7] = shiftingTemp;
+            shiftingTemp = 0;
 
-int shiftingTemp = 0;
-void shiftRow(int row_num){
-    //If it is the second row it shifts all the values so that the first value of the row is moved to the end and the
-    //rest are shifted to the left one.
-    if (row_num == 2){
-        shiftingTemp = matrixValue[5];
-        matrixValue[5] = matrixValue[6];
-        matrixValue[6] = matrixValue[7];
-        matrixValue[7] = matrixValue[8];
-        matrixValue[8] = shiftingTemp;
-        shiftingTemp = 0;
+            //Swaps the first and third values of the row and the second and fourth of the row as well.
+        } else if (row_num == 3){
+            shiftingTemp = state[8];
+            state[8] = state[10];
+            state[10] = shiftingTemp;
+            shiftingTemp = state[9];
+            state[9] = state[11];
+            state[11] = shiftingTemp;
+            shiftingTemp = 0;
 
-    //Swaps the first and third values of the row and the second and fourth of the row as well.
-    } else if (row_num == 3){
-        shiftingTemp = matrixValue[9];
-        matrixValue[9] = matrixValue[11];
-        matrixValue[11] = shiftingTemp;
-        shiftingTemp = matrixValue[10];
-        matrixValue[10] = matrixValue[12];
-        matrixValue[12] = shiftingTemp;
-        shiftingTemp = 0;
-
-    //If it is the fourth row it shifts all the values so that the last value of the row is moved to the beginning and
-    //the rest are shifted to the right one.
-    } else if (row_num == 4){
-        shiftingTemp = matrixValue[16];
-        matrixValue[14] = matrixValue[13];
-        matrixValue[15] = matrixValue[14];
-        matrixValue[16] = matrixValue[15];
-        matrixValue[13] = shiftingTemp;
-        shiftingTemp = 0;
+            //If it is the fourth row it shifts all the values so that the last value of the row is moved to the beginning and
+            //the rest are shifted to the right one.
+        } else if (row_num == 4){
+            shiftingTemp = state[15];
+            state[15] = state[14];
+            state[14] = state[13];
+            state[13] = state[12];
+            state[12] = shiftingTemp;
+            shiftingTemp = 0;
+        }
+        return state[0];
     }
-}
+};
 
 #endif //AES_SHIFT_ROWS_HPP
